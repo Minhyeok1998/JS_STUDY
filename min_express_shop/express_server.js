89f4932369d0f316699a450f1293f325c750e0c7
Minhyeok1998 <tom2945000@gmail.com>
2022-03-30 17:26:17 +0900

    login page -> admin main page 넘어가기 완료

--- min_express_shop/express_server.js
+++ min_express_shop/express_server.js
@@ -27,6 +27,11 @@ app.use(express.static('public')); //static resource 요청이 들어오면 publ
 
 let login_id =''; //login 화면에 login 이 성공하게 되면 DB에 해당 튜플의 애트리뷰트 LOGIN ='on' 으로 바뀌고 , id를 login_id에 저장한다. 
 
+
+//!!!!!!!!!!!!!!!!!!!!!!!!setInterval 로 30분 마다 login_id 를 초기화  구현 해야한다 !!!!!!!!!!!!!!!!!!!!(구현하면 지우기)
+setInterval(()=>{
+    login_id = '';
+},2100000);
 app.get('/admin',async (req,res)=>{
     
     let query = `SELECT LOGIN FROM ADMIN WHERE ID = ?`;
@@ -38,6 +43,9 @@ app.get('/admin',async (req,res)=>{
         if(db_res['result'][0].LOGIN == 'on'){
             console.log("로그인 되어 있음");
             //관리자 메인 페이지를 전송!!
+            const admin_main_page = await readData('public/admin/html/admin_main.html');
+            res.write(admin_main_page);
+            res.send();
         }else{
            console.log("로그인 되어 있지 않음");
            //세션 만료 다시 로그인 해주세요!
@@ -48,16 +56,17 @@ app.get('/admin',async (req,res)=>{
                 alert(LOGIN_message); 
             </script>
            `);
+           login_id = '';
            let login_page = readData('public/admin/html/admin_login.html');
            login_page = await login_page;
            res.write(login_page);
            res.send();
         }
     }else{
-        console.log("없는 아이디!");
+        console.log("login_id에 값이 없음");
         res.write(`
             <script>
-                const LOGIN_message = 'login 정보를 다시 입력해주세요!!';
+                const LOGIN_message = 'login 해주세요!';
                 alert(LOGIN_message);
             </script>
         `);
@@ -69,10 +78,51 @@ app.get('/admin',async (req,res)=>{
     }
 });
 
-app.post('/admin/login.do',(req,res)=>{
-    console.log('hi form');
-    console.log(req.body);
+app.post('/admin/login.do',async (req,res)=>{
+    console.log("LOGIN FORM 에서 SUBMIT 이벤트 발생 했당~~");
+    // console.log(req.body);
+    const ID = req.body.id; 
+    const PWD = req.body.pwd;
+   
+    let login_sql = 'SELECT COUNT(*) as "login_result" FROM ADMIN WHERE ID= ? AND PASSWORD = ?';
+    let db_result = await connMysqlQuery(login_sql,[ID,PWD]);
 
+    console.log(db_result['result']); 
+    //db_result['result'] [{login_result:1}] 배열 의 object로 온다.
+    if(db_result['result'][0].login_result == 1){ // LOGIN 결과
+        console.log("LOGIN 성공");
+        //관리자 화면으로 이동.
+        //login_id 에 ID를 저장한다.
+        const adminMain_page = await readData('public/admin/html/admin_main.html');
+        res.write(`
+            <script>
+                const LOGIN_ID = ${ID};
+                alert("로그인 성공 ~");
+            </script>
+        `)
+        res.write(adminMain_page);
+        login_id = ID;
+        let update_query = 'UPDATE ADMIN SET LOGIN = "on" WHERE ID=?';
+        const update_admin_login = await connMysqlQuery(update_query,[ID]);
+        if(update_admin_login['result'].affectedRows == 1){
+            console.log("admin table 에서 LOGIN ='on'으로 성공함");
+        }
+    }else{
+        console.log("LOGIN 실패");
+        //로그인 화면으로 이동
+        //alert("ID 또는 Password를 확인 해주세요");
+        
+        const Login_page = await readData('public/admin/html/admin_login.html');
+        res.write(`
+            <script>
+                alert("ID 또는 Password를 확인 해주세요");
+            </script>
+        `);
+        res.write(Login_page);
+    }
+
+    db_result['conn'].end();
+    res.send();
 })
 
 app.listen(8877, (e)=>{
